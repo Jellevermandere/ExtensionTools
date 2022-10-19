dce7cad632bbccf079b1244eb653b69988276cac
Jelle Vermandere <jelle.vermandere@gmail.com>
2022-10-19 21:55:44 +0200

    SerialisedUpdate

--- Runtime/EventExtensions.cs
+++ Runtime/EventExtensions.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 
 namespace JelleVer.ExtensionTools
 {
+    [System.Serializable]
     public class IntEvent : UnityEvent<int> { }
-
+    [System.Serializable]
     public class FloatEvent : UnityEvent<float> { }
-
+    [System.Serializable]
     public class StringEvent : UnityEvent<string> { }
-
+    [System.Serializable]
     public class ColliderEvent : UnityEvent<Collider> { }
 
 
